d078bc4a88f97a8a0775a92543cdb8f928ad38d6
unknown <daniel.gomez-su@labinfo.is.escuelaing.edu.co>
2020-02-12 12:15:36 -0500

    implementacion del modelo

--- src/test/java/hangman/test/HangmanTest.java
+++ src/test/java/hangman/test/HangmanTest.java
@@ -50,19 +50,123 @@ public class HangmanTest {
     
         
     @Test
-    public void validateOriginalScore (){
+    public void validateOriginalScorecorrect (){
         try{
             OriginalScore sc =new OriginalScore();
             int resultado = sc.calculateScore(10, 3);
-            
+            Assert.assertEquals(70,resultado);
         }catch ( HangmanException e){
             
             
-        }
+        }  
+    }
     
-       
+    @Test
+    public void validateOriginalScorecorrectSup (){
+        try{
+            OriginalScore sc =new OriginalScore();
+            int resultado = sc.calculateScore(3, 0);
+            Assert.assertEquals(100,resultado);
+        }catch ( HangmanException e){        
+        }  
+    }
+    
+    @Test
+    public void validateOriginalScorecorrectInf (){
+        try{
+            OriginalScore sc =new OriginalScore();
+            int resultado = sc.calculateScore(10, 11);
+            Assert.assertEquals(0,resultado);
+        }catch ( HangmanException e){  
+        } 
     }
     
     
+    @Test
+    public void validateOriginalScoreInParam (){
+        try{
+            OriginalScore sc =new OriginalScore();
+            int resultado = sc.calculateScore(-1,3);  
+        }catch ( HangmanException e){
+            Assert.assertTrue(true);
+        }  
+    }
+ /* -------------------------------------------------- BONUS SCORE ----------------------------------------------*/  
+    @Test
+    public void validateBonusScorecorrect (){
+        try{
+            BonusScore sc =new BonusScore();
+            int resultado = sc.calculateScore(3,5);
+            Assert.assertEquals(5,resultado);
+        }catch ( HangmanException e){
+            
+            
+        }  
+    }
+   
+    @Test
+    public void validateBonusScorecorrectInf (){
+        try{
+            BonusScore sc =new BonusScore();
+            int resultado = sc.calculateScore(2, 6);
+            Assert.assertEquals(0,resultado);
+        }catch ( HangmanException e){  
+        } 
+    }
+    
+    
+    @Test
+    public void validateBonusScoreInParam (){
+        try{
+            BonusScore sc =new BonusScore();
+            int resultado = sc.calculateScore(-11,-6);  
+        }catch ( HangmanException e){
+            Assert.assertTrue(true);
+        }  
+    }  
+    
+    /* --------------------------- POWER SCORE ----------------------*/
+       @Test
+    public void validatePowerScorecorrect (){
+        try{
+            PowerScore sc =new PowerScore();
+            int resultado = sc.calculateScore(2, 3);
+            Assert.assertEquals(1,resultado);
+        }catch ( HangmanException e){
+            
+            
+        }  
+    }
+    
+    @Test
+    public void validatePowerScorecorrectSup (){
+        try{
+            PowerScore sc =new PowerScore();
+            int resultado = sc.calculateScore(4, 0);
+            Assert.assertEquals(500,resultado);
+        }catch ( HangmanException e){        
+        }  
+    }
+    
+    @Test
+    public void validatePowerScorecorrectInf (){
+        try{
+            PowerScore sc =new PowerScore();
+            int resultado = sc.calculateScore(1, 3);
+            Assert.assertEquals(0,resultado);
+        }catch ( HangmanException e){  
+        } 
+    }
+    
+    
+    @Test
+    public void validatePowerScoreInParam (){
+        try{
+            PowerScore sc =new PowerScore();
+            int resultado = sc.calculateScore(8,-11);  
+        }catch ( HangmanException e){
+            Assert.assertTrue(true);
+        }  
+    }
     
 }
